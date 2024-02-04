9b227c041a65c6a2c3a878f7756d788349a78860
bgenia <bgenia@student.21-school.ru>
2024-02-04 14:35:32 +0300

    feat: implement webpageUrl argument for `sources`, replace default out dir name with webpage name

--- src/cli/commands/sources/index.ts
+++ src/cli/commands/sources/index.ts
@@ -1,4 +1,4 @@
-import { command, option } from "cmd-ts"
+import { command, option, optional, positional, string } from "cmd-ts"
 import { createHash } from "node:crypto"
 import { join } from "node:path"
 import { NewFile } from "@/cli/arguments/types/new-file"
@@ -15,34 +15,35 @@ export const sourcesCommand = command({
 	name: "sources",
 	description: "Walks and downloads all JS sources from the platform",
 	args: {
+		webpageUrl: positional({
+			type: optional(string),
+			description: "Webpage to download sources from",
+			displayName: "webpage url",
+		}),
 		outDir: option({
 			long: "out-dir",
 			short: "o",
-			type: NewFile,
+			type: optional(NewFile),
 			description: "Directory for downloaded sources",
-			defaultValue: () => "edu-src",
+			defaultValue: () => undefined,
 		}),
 	},
 	async handler(argv) {
-		const { outDir } = argv
+		const webpageUrl = argv.webpageUrl ?? Constants.Platform.BASE_URL
+		const outDir = argv.outDir ?? webpageUrl.replaceAll(/[^\w\d]+/g, "-")
 
 		const writePromises: Promise<unknown>[] = []
 
-		await walkScriptsFromWebpage(
-			Constants.Platform.BASE_URL,
-			(_program, text, source) => {
-				const url = new URL(source)
+		await walkScriptsFromWebpage(webpageUrl, (_program, text, source) => {
+			const url = new URL(source)
 
-				const filename =
-					url.pathname === "/"
-						? createRootScriptFilename(text)
-						: url.pathname
+			const filename =
+				url.pathname === "/" ? createRootScriptFilename(text) : url.pathname
 
-				const writePromise = Bun.write(join(outDir, filename), text)
+			const writePromise = Bun.write(join(outDir, filename), text)
 
-				writePromises.push(writePromise)
-			},
-		)
+			writePromises.push(writePromise)
+		})
 
 		await Promise.all(writePromises)
 	},
