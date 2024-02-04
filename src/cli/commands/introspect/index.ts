9f12047f6839bb779854325d7972bca699a73205
bgenia <bgenia@student.21-school.ru>
2024-02-04 15:56:09 +0300

    feat: implement har loading in `introspect`

--- src/cli/commands/introspect/index.ts
+++ src/cli/commands/introspect/index.ts
@@ -1,21 +1,97 @@
-import { command, flag, option } from "cmd-ts"
+import { Node } from "acorn"
+import { command, flag, option, optional, positional } from "cmd-ts"
 import { source } from "common-tags"
 import { printSchema } from "graphql"
+import { Har } from "har-format"
 import { resolve } from "node:path"
 import { PLATFORM_TOKEN } from "@/cli/arguments/platform-token"
+import { HarFile } from "@/cli/arguments/types/har-file"
 import { NewFile } from "@/cli/arguments/types/new-file"
 import { extractGqlLiterals } from "@/common/extract-gql-literals"
 import { fetchStaticProperties } from "@/common/fetch-static-properties"
 import { fetchTypeSchema } from "@/common/fetch-type-schema"
-import { walkScriptsFromWebpage } from "@/common/walk-scripts"
+import {
+	fetchText,
+	walkScriptsFromScript,
+	walkScriptsFromWebpage,
+} from "@/common/walk-scripts"
 import { Constants } from "@/constants"
 
-async function fetchGraphqlLiterals() {
+function getHarTextEntries(har: Har, mimeType: string) {
+	return new Map(
+		har.log.entries
+			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+			.filter((entry) => entry.response.content.mimeType?.includes(mimeType))
+			.map((entry) => [entry.request.url, entry.response.content.text!]),
+	)
+}
+
+async function fetchGqlLiterals(har?: Har) {
 	const gqlLiterals: string[] = []
 
-	await walkScriptsFromWebpage(Constants.Platform.BASE_URL, (program) => {
+	const loadedDocuments = har
+		? getHarTextEntries(har, "html")
+		: new Map<string, string>()
+
+	const loadedScripts = har
+		? getHarTextEntries(har, "javascript")
+		: new Map<string, string>()
+
+	if (har) {
+		console.log("HAR loaded:")
+		console.log("\tDocuments:", loadedDocuments.size)
+		console.log("\tScripts:", loadedScripts.size)
+	}
+
+	async function getOrFetchText(url: string) {
+		if (loadedDocuments.has(url)) {
+			return loadedDocuments.get(url)!
+		}
+
+		if (loadedScripts.has(url)) {
+			return loadedScripts.get(url)!
+		}
+
+		return await fetchText(url)
+	}
+
+	const documentUrls = new Set([
+		Constants.Platform.BASE_URL,
+		...loadedDocuments.keys(),
+	])
+
+	const visitedSources = new Set<string>()
+
+	function scrapQueries(program: Node, _text: string, source: string) {
+		console.log(
+			`Scrapping queries from: ${source}`,
+			loadedScripts.has(source) ? "-> Loaded from HAR" : "",
+		)
+
 		gqlLiterals.push(...extractGqlLiterals(program))
-	})
+	}
+
+	for (const url of documentUrls) {
+		const visitedScripts = await walkScriptsFromWebpage(url, scrapQueries, {
+			visitedSources,
+			fetchText: getOrFetchText,
+		})
+
+		for (const script of visitedScripts) {
+			visitedSources.add(script)
+		}
+	}
+
+	for (const url of loadedScripts.keys()) {
+		const visitedScripts = await walkScriptsFromScript(url, scrapQueries, {
+			visitedSources,
+			fetchText: getOrFetchText,
+		})
+
+		for (const script of visitedScripts) {
+			visitedSources.add(script)
+		}
+	}
 
 	return gqlLiterals
 }
@@ -57,16 +133,23 @@ export const introspectCommand = command({
 			description: "Only fetch types (and not queries)",
 			defaultValue: () => false,
 		}),
+		har: positional({
+			description: "HAR file to infer queries from",
+			displayName: "har file",
+			type: optional(HarFile),
+		}),
 	},
 	async handler(argv) {
-		const { accessToken, outFile, typesOnly } = argv
+		const { accessToken, outFile, typesOnly, har } = argv
+
+		console.log("Fetching schema")
 
 		const [typeSchema, staticProperties] = await Promise.all([
 			fetchTypeSchema(accessToken),
 			fetchStaticProperties(),
 		])
 
-		const gqlLiterals = typesOnly ? [] : await fetchGraphqlLiterals()
+		const gqlLiterals = typesOnly ? [] : await fetchGqlLiterals(har)
 
 		const resolvedOutFile = resolveOutFile(outFile, staticProperties)
 
