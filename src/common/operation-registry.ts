52f3e4c09c5225ac57ff9440e97d27d679212e6c
bgenia <bgenia@student.21-school.ru>
2024-02-05 05:27:09 +0300

    feat: implement OperationRegistry

---
+++ src/common/operation-registry.ts
@@ -0,0 +1,104 @@
+import {
+	DocumentNode,
+	FragmentDefinitionNode,
+	Kind,
+	OperationDefinitionNode,
+	parse,
+	visit as walk,
+} from "graphql"
+import { createHash } from "node:crypto"
+import { match, P } from "ts-pattern"
+
+type Operation = {
+	definition: OperationDefinitionNode
+	fragmentReferences: Set<string>
+}
+
+export class OperationRegistry {
+	#fragments = new Map<string, FragmentDefinitionNode>()
+	#operations = new Map<string, Operation>()
+
+	#addFragmentDefinition(definition: FragmentDefinitionNode) {
+		this.#fragments.set(definition.name.value, definition)
+	}
+
+	#addOperationDefinition(definition: OperationDefinitionNode) {
+		const name = match(definition)
+			.with(
+				{
+					name: {
+						kind: Kind.NAME,
+						value: P.string.select("name"),
+					},
+				},
+				({ name }) => name,
+			)
+			.otherwise((node) =>
+				createHash("md5").update(JSON.stringify(node)).digest("hex"),
+			)
+
+		const fragmentReferences = new Set<string>()
+
+		walk(definition, {
+			FragmentSpread: (node) =>
+				match(node).with(
+					{
+						kind: Kind.FRAGMENT_SPREAD,
+						name: {
+							kind: Kind.NAME,
+							value: P.string.select("name"),
+						},
+					},
+					({ name }) => {
+						fragmentReferences.add(name)
+					},
+				),
+		})
+
+		this.#operations.set(name, {
+			definition,
+			fragmentReferences,
+		})
+	}
+
+	addLiteral(literal: string) {
+		let node
+
+		try {
+			node = parse(literal)
+		} catch {
+			return false
+		}
+
+		walk(node, {
+			OperationDefinition: (node) => this.#addOperationDefinition(node),
+			FragmentDefinition: (node) => this.#addFragmentDefinition(node),
+		})
+
+		return true
+	}
+
+	*getValidOperations() {
+		for (const [_name, operation] of this.#operations) {
+			const fragmentReferences = Array.from(operation.fragmentReferences)
+			const hasAllFragments = fragmentReferences.every((fragment) =>
+				this.#fragments.has(fragment),
+			)
+
+			if (!hasAllFragments) {
+				continue
+			}
+
+			const fragmentDefinitions = fragmentReferences.map(
+				(fragment) => this.#fragments.get(fragment)!,
+			)
+
+			const node: DocumentNode = {
+				kind: Kind.DOCUMENT,
+				definitions: [...fragmentDefinitions, operation.definition],
+			}
+
+			yield node
+		}
+	}
+}
