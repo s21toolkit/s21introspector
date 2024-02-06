50b04a374cade187a8f7b73233b5848f80a9374d
bgenia <bgenia@student.21-school.ru>
2024-02-06 20:34:22 +0300

    feat: implement edu-admin scraping

--- src/common/extract-gql-literals.ts
+++ src/common/extract-gql-literals.ts
@@ -6,25 +6,37 @@ import { match, P } from "ts-pattern"
 export function extractGqlLiterals(program: Node) {
 	const gqlLiterals: string[] = []
 
+	function tryCollectGqlLiteral(literal: string) {
+		const trimmedValue = literal.trim()
+
+		if (trimmedValue.startsWith("{")) {
+			return
+		}
+
+		try {
+			parseGQL(literal)
+
+			gqlLiterals.push(literal)
+		} catch {}
+	}
+
 	walkProgram(program, {
 		Literal: (node) =>
 			match(node).with(
 				{
 					value: P.string.minLength(1).select("value"),
 				},
-				({ value }) => {
-					const trimmedValue = value.trim()
-
-					if (trimmedValue.startsWith("{")) {
-						return
-					}
-
-					try {
-						parseGQL(value)
-
-						gqlLiterals.push(value)
-					} catch {}
+				({ value }) => tryCollectGqlLiteral(value),
+			),
+		TemplateElement: (node) =>
+			match(node).with(
+				{
+					type: "TemplateElement",
+					value: {
+						cooked: P.string.select("value"),
+					},
 				},
+				({ value }) => tryCollectGqlLiteral(value),
 			),
 	})
 
--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,9 @@ export namespace Constants {
 		export const GQL_URL = `${BASE_URL}/services/graphql`
 
 		export const STATIC_SCRIPT_URL = `${BASE_URL}/static.js`
+
+		export namespace Admin {
+			export const BASE_URL = "https://edu-admin.21-school.ru"
+		}
 	}
 }
